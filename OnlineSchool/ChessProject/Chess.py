7cc9546c6ce8e75453728cc11a393350e1a2dbdc
User <sergusmisha@gmail.com>
2025-07-10 17:50:55 +0300

    project(Chess)v_2

--- OnlineSchool/ChessProject/Chess.py
+++ OnlineSchool/ChessProject/Chess.py
@@ -2,3 +2,16 @@ import numpy as np
 import tkinter as tk
 import random
 import math
+
+class Figure:
+    def __init__(self, color, is_alive, x_cords):
+        self.color = color
+        self.is_alive = is_alive
+
+        list_of_y_coords = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        self.x_cords = x_cords
+
+
+
+
+
