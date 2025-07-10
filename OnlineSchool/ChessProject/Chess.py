34e8aabdee5190c36ac4981309d0ec691e8b47cd
User <sergusmisha@gmail.com>
2025-07-10 20:27:47 +0300

    project(Chess)v_3_0

--- OnlineSchool/ChessProject/Chess.py
+++ OnlineSchool/ChessProject/Chess.py
@@ -9,7 +9,7 @@ from matplotlib.pyplot import figure
 class Figure: #Створюю матер клас фігури
     list_of_x_coords = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']#Масив з клітинками створений саме в класі а не
     #в об'єкті
-    def __init__(self, canvas, game_ref, color, is_alive, y_cord, x_letter):#що незрозумілого це
+    def __init__(self, canvas, game_ref, color, y_cord, x_letter, is_alive = True):#що незрозумілого це
         # конструктор іди гортай
         self.canvas = canvas#канвас це типу вікно
         self.game = game_ref#це буде клас гри
@@ -33,24 +33,27 @@ class Figure: #Створюю матер клас фігури
             center_y,
             text="",
             fill=self.color,
-            font=("Arial Black", 24)
+            font=("Arial Black", 35)
         )
 
 class Pawn(Figure):
-    def __init__(self, canvas, game_ref, color, is_alive, y_cord, x_letter):
-        super().__init__(canvas, game_ref, color, is_alive, y_cord, x_letter)#конструктор успадкування
-        self.canvas.itemconfig(self.figure_id, text="P♙")# тут ми візуалізуємо ппішака типу в нього тепер є текст
+    def __init__(self, canvas, game_ref, color, y_cord, x_letter, is_alive = True):
+        super().__init__(canvas, game_ref, color, y_cord, x_letter, is_alive)#конструктор успадкування
+        self.canvas.itemconfig(self.figure_id, text="♙")# тут ми візуалізуємо ппішака типу в нього тепер є текст
 
 class Chess:#клас гри
     def __init__(self,root):
         self.root = root#сам ткшний рут
         self.root.title("Chess")#заголовок
 
+        self.board = [[None for _ in range(8)] for _ in range(8)]
+
         self.canvas = tk.Canvas(root, width=600, height=600, bg="#F5DEB3", highlightthickness=3,
-                                highlightbackground="black")#Той самий легендарний канвас(леонардо малював монолізу тут)
+                                highlightbackground="#F5DEB3")#Той самий легендарний канвас(леонардо малював монолізу тут)
         self.canvas.pack()#Розпаковую його в рут
 
         self.draw_board()#малюю чорні клітинки
+        self.draw_figures()
 
     def draw_board(self):
         cell_size_x = int(self.canvas['width']) / 8#розраховую розмір клітинки
@@ -72,6 +75,18 @@ class Chess:#клас гри
                         y2,
                         fill = '#8B4513'
                     )
+    def draw_figures(self):
+        #black Rook
+        #black Bishop
+        #black Knight
+        #black Queen
+        #black King
+        #black Knight
+        #black Bishop
+        #black Rook
+        for i in range(8):
+            self.board[1][i] = Pawn(self.canvas, self, 'black',6,Figure.list_of_x_coords[i])
+            self.board[7][i] = Pawn(self.canvas, self, 'white', 1, Figure.list_of_x_coords[i])
 
 if __name__ == "__main__":#якщо цей проект буде бібліотекою то все що нище не запуститься
     root = tk.Tk()#створюю вікно в тк
