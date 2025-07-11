804b425209d740f34369d70834f75cd7947f3f4e
User <sergusmisha@gmail.com>
2025-07-11 16:43:19 +0300

    project(Chess)v_3_4

--- OnlineSchool/ChessProject/Chess.py
+++ OnlineSchool/ChessProject/Chess.py
@@ -20,28 +20,78 @@ class Figure: #Створюю матер клас фігури
         self.y_cord = y_cord
         self.x_cord = Figure.list_of_x_coords.index(x_letter)#тут я беру букву та конвертую в індекс
 
-        cell_width = float(self.canvas['width']) / 8#розраховую ширину клітинки
-        cell_height = float(self.canvas['height']) / 8
-
-        center_x = self.x_cord * cell_width + cell_width / 2
+        self.center_x = self.x_cord * self.game.cell_width + self.game.cell_width / 2
         # Координати канваса починаються з лівого верхнього кута
         # ми спочатку розраховуємо довж 1 кліт потім множимо на інд кліт а потім від шир канв відн
         # значення та ще половину кліт щоб текст був посередині
-        center_y = (float(self.canvas['height']) - cell_height * self.y_cord) - (cell_height / 2)
+        self.center_y = (float(self.canvas['height']) - self.game.cell_height * self.y_cord) - (self.game.cell_height / 2)
 
         self.figure_id = self.canvas.create_text(#ця шняга малює на канвасі текст покищо тексту нема бо це конст
-            center_x,
-            center_y,
+            self.center_x,
+            self.center_y,
             text="",
             fill=self.color,
             font=("Arial Black", 35)
         )
+
+    def show_variants(self):
+        pass
+
+    def delete_variants(self):
+        pass
+    #self.canvas.delete(self.points[0])
 
 class Pawn(Figure):
     def __init__(self, canvas, game_ref, color, y_cord, x_letter, is_alive = True, is_active = False, have_ever_moved = False):
         super().__init__(canvas, game_ref, color, y_cord, x_letter, is_alive, is_active)#конструктор успадкування
         self.canvas.itemconfig(self.figure_id, text="♙")# тут ми візуалізуємо ппішака типу в нього тепер є текст
         self.have_ever_moved = have_ever_moved#Типу це спец штука для пішака бо пішак спочатку може ходити на 2 вперед замість 1
+        self.variants = []
+        self.points = []
+
+    def show_variants(self):
+        self.points = []
+        for i in range(1,3):
+            if self.game.board[self.y_cord + i][self.x_cord] is None and i == 1:
+                self.points.append(self.canvas.create_oval(
+                    self.center_x - 5, self.center_y - 5 - self.game.cell_height * i,
+                    self.center_x + 5, self.center_y + 5 - self.game.cell_height * i,
+                    fill="gray",
+                    outline=""  # прибираю чорну обводку
+                ))
+            elif self.game.board[self.y_cord + i][self.x_cord] is None and not self.have_ever_moved:
+                self.points.append(self.canvas.create_oval(
+                    self.center_x - 5, self.center_y - 5 - self.game.cell_height * 2,
+                    self.center_x + 5, self.center_y + 5 - self.game.cell_height * 2,
+                    fill="gray",
+                    outline=""  # прибираю чорну обводку
+                ))
+            else:
+                break
+            self.variants.append([self.y_cord + i, self.x_cord])
+        obj = self.game.board[self.y_cord + 1][self.x_cord - 1]
+        if (obj is not None and
+                ((obj.color == 'black' and self.game.white_moving) or
+                (obj.color == 'white' and not self.game.white_moving))):
+            self.points.append(self.canvas.create_oval(
+                self.center_x - 5 - self.game.cell_width, self.center_y - 5 - self.game.cell_height,
+                self.center_x + 5 - self.game.cell_width, self.center_y + 5 - self.game.cell_height,
+                fill="gray",
+                outline=""  # прибираю чорну обводку
+            ))
+            self.variants.append([self.y_cord + 1, self.x_cord + 1])
+        obj = self.game.board[self.y_cord + 1][self.x_cord + 1]
+        if (obj is not None and
+                ((obj.color == 'black' and self.game.white_moving) or
+                (obj.color == 'white' and not self.game.white_moving))):
+            self.points.append(self.canvas.create_oval(
+                self.center_x - 5 + self.game.cell_width, self.center_y - 5 - self.game.cell_height,
+                self.center_x + 5 + self.game.cell_width, self.center_y + 5 - self.game.cell_height,
+                fill="gray",
+                outline=""  # прибираю чорну обводку
+            ))
+            self.variants.append([self.y_cord + 1, self.x_cord + 1])
+        print(self.variants)
 
 class Chess:#клас гри
 
@@ -62,10 +112,12 @@ class Chess:#клас гри
         # B B B B B B B B
         # B B B B B B B B
 
-
         self.canvas = tk.Canvas(root, width=640, height=640, bg="#F5DEB3", highlightthickness=3,
                                 highlightbackground="black")#Той самий легендарний канвас(леонардо малював монолізу тут)
         self.canvas.pack(side = tk.LEFT)#Розпаковую його в рут
+
+        self.cell_width = float(self.canvas['width']) / 8  # розраховую ширину клітинки
+        self.cell_height = float(self.canvas['height']) / 8
 
         self.draw_board()#малюю чорні клітинки
         self.draw_figures()#фігури
@@ -76,18 +128,16 @@ class Chess:#клас гри
         self.canvas.bind("<Button-1>", self.click) # При натисканні лівої кнопки буде виклик self.click
 
     def draw_board(self):
-        cell_size_x = float(self.canvas['width']) / 8#розраховую розмір клітинки
-        cell_size_y = float(self.canvas['height']) / 8
 
 
         for i in range(8):
             for j in range(8):
                 if (i + j) % 2 != 1:#перевіряю чи клітинка повинна бути чорною
-                    x1 = j * cell_size_x#далі треба буде мати лівий верхній та правий нижній коорд клітинки(коорд поч з лівої
+                    x1 = j * self.cell_width#далі треба буде мати лівий верхній та правий нижній коорд клітинки(коорд поч з лівої
                     # верхньої частини жошки)
-                    y1 = float(self.canvas['height']) - (i * cell_size_y) - cell_size_y
-                    x2 = x1 + cell_size_x
-                    y2 = y1 + cell_size_y
+                    y1 = float(self.canvas['height']) - (i * self.cell_height) - self.cell_height
+                    x2 = x1 + self.cell_width
+                    y2 = y1 + self.cell_height
                     self.canvas.create_rectangle(#створюю клітинку
                         x1,
                         y1,
@@ -105,7 +155,8 @@ class Chess:#клас гри
         # black Bishop
         # black Rook
         for i in range(8):#Pawns
-            self.board[6][i] = Pawn(self.canvas, self, 'black',6,Figure.list_of_x_coords[i])#створюю пішаків
+            x = random.randint(2,5)
+            self.board[x][i] = Pawn(self.canvas, self, 'black',x,Figure.list_of_x_coords[i])#створюю пішаків
             self.board[1][i] = Pawn(self.canvas, self, 'white', 1, Figure.list_of_x_coords[i])
         # white Bishop
         # white Knight
@@ -117,22 +168,22 @@ class Chess:#клас гри
 
     def click(self,event):
         if self.first_click is None:# Перевірка чи є клік (першим) якщо не вибрана ніяка фігура
-            cell_size_x = float(self.canvas['width']) / 8  # розраховую розмір клітинки
-            cell_size_y = float(self.canvas['height']) / 8
-
-
-            col = int(event.x / cell_size_x)#Рахую номер колонки
-            row = 7 - int(event.y / cell_size_y)#Рахую номен рядка
+            row = 7 - int(event.y / self.cell_height)  # Рахую номен рядка
+            col = int(event.x / self.cell_width)#Рахую номер колонки
             self.first_click = (row, col)# задаю у змінну коорд на дошці щоб потім ми знали з якою фігурою взаємодіємо
             if self.board[row][col] is not None:# перевіряю чи є на клітинці фігура
                 on_cl = self.board[row][col]# щоб не звертатися до дошки роблю змінну
                 if ((self.white_moving and on_cl.color == 'white') or
                     (not self.white_moving and on_cl.color == 'black')):#білі взаємодіють з білими а чорні з чорними
                     on_cl.is_active = True# роблю фігуру активною
+                    on_cl.show_variants()
                 else:
                     self.first_click = None#не вийде вибрати інший колір
             else:
                 self.first_click = None#не вийде походити пустотою
+        else:
+            on_cl = self.board[self.first_click[0]][self.first_click[1]]
+            self.first_click = None
 
 if __name__ == "__main__":#якщо цей проект буде бібліотекою то все що нище не запуститься
     root = tk.Tk()#створюю вікно в тк
